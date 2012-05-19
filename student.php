9a32cf942ab7e391f10a184e3bd95365051a1cb3
Romack Natividad <romack@wizmediateam.com>
2012-05-19 20:23:09 +0800

    defensive programming

--- student.php
+++ student.php
@@ -25,10 +25,10 @@ if(!empty($_POST)) {
 <? if(!empty($student['id'])):?>
 <input type="hidden" name="id" value="<?=$student['id']?>" />
 <? endif;?>
-First Name: <input type="text" name="first_name" value="<?=$student['first_name']?>" /><br />
-Middle Name: <input type="text" name="middle_name" value="<?=$student['middle_name']?>" /><br />
-Last Name: <input type="text" name="last_name" value="<?=$student['last_name']?>" /><br />
-Email: <input type="text" name="email" value="<?=$student['email']?>" /><br />
+First Name: <input type="text" name="first_name" value="<?=@$student['first_name']?>" /><br />
+Middle Name: <input type="text" name="middle_name" value="<?=@$student['middle_name']?>" /><br />
+Last Name: <input type="text" name="last_name" value="<?=@$student['last_name']?>" /><br />
+Email: <input type="text" name="email" value="<?=@$student['email']?>" /><br />
 <input type="submit" value="Save" /><br />
 <a href="students.php">Cancel</a>
 </form>
